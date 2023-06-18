a75158bf82ca85f824f52320ea996776530ef097
dhruvitpatel <dhruvitpatel@knovator.in>
2023-06-18 19:10:35 +0530

    added meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,18 @@ import "@/styles/tailwind.css";
 import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <meta
+          name="keywords"
+          content="Dhruvit Patel, dhruvit patel, dhruvitpatel, dhruvit patel web designer, best web designer in kamrej, best web designer in surat, best web designer in pasodara, web designer, web developer"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
